357988045a9c3c74d973b350bf5556ed8ee83eae
MarkoDev <Marcosjmh.dev@gmail.com>
2024-07-26 16:22:21 -0400

    InterfaceCompleted

---
+++ DApp.Data/Interfaces/ICourseDb.cs
@@ -0,0 +1,18 @@
+﻿using DApp.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DApp.Data.Interfaces
+{
+    public interface ICourseDb
+    {
+        List<Course> GetCourses();
+        Course GetCourse(int Id);
+        void SaveCourse(Course course);
+        void UpdateCourse(Course course);
+        void RemoveCourse(Course course);
+    }
+}
